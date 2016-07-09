4304618a5f21f7a6159aeffff37bb5ecaffb180e
Adam Worrall <adam@worrall.cc>
2016-07-09 10:07:12 -0700

    Handle script output better, and use hostname to select specific scripts

--- skypi/updater.pl
+++ skypi/updater.pl
@@ -4,9 +4,11 @@ use warnings;
 use strict;
 
 use POSIX qw(strftime);
+use Sys::Hostname;
 
-our $workdir = "$ENV{HOME}/skypi/.update";
-our $updater = "updater";
+our $workdir = "$ENV{HOME}/.update";
+our $hostname = hostname;
+our $updater = "updater.$hostname";
 
 eval {
     (-d $workdir) || mkdir($workdir) || die "mkdir $workdir: $!\n";
@@ -22,14 +24,19 @@ eval {
         exit 0;
     }
 
+    my $outfile = "updater.".strftime("%Y%m%d-%H%M%S",gmtime(time()));
     chmod(0755, $updater);
-    system("./$updater");
+    system("./$updater 1>$outfile 2>&1");
 
+    # Roll scripts
     if (-f "$updater.last") {
-        my $suffix = strftime("%Y%m%d-%T", gmtime((stat "$updater.last")[9]));
+        my $suffix = strftime("%Y%m%d-%H%M%S", gmtime((stat "$updater.last")[9]));
         rename("$updater.last", "$updater.$suffix")
     }
     rename ($updater, "$updater.last");
+
+    my $destfile = "skypi\@wormers.net:~/$hostname";
+    `scp $outfile $destfile`;
 
 }; if ($@) {
     print "Aborted with: $@\n";
